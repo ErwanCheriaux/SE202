e39fe7b1db385654cba19ec716b409e2703c7453
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-03-10 11:42:18 +0100

    Update Binder

--- semantics/binder.py
+++ semantics/binder.py
@@ -121,5 +121,7 @@ class Binder(Visitor):
 
     @visitor(FunCall)
     def visit(self, fun):
+        if type(fun.identifier.decl) is not FunCall:
+            raise BindException("Is not a function: %s" % fun.identifier.name)
         if len(self.lookup(fun.identifier).args) != len(fun.params):
             raise BindException("Not the right number of param for the function: %s" % fun.identifier.name)
