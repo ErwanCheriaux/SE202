854608d96fd656d4ca2414278a53c405b9242826
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-03-12 23:42:07 +0100

    Update test dumper

--- parser/test_dumper.py
+++ parser/test_dumper.py
@@ -57,19 +57,23 @@ class TestDumper(unittest.TestCase):
         self.check("1+2<>3", "((1 + 2) <> 3)")
         self.check("2<>3+1", "(2 <> (3 + 1))")
 
+    def test_ifthenelse(self):
         self.check("if 5=5 then 2 else 3", "if (5 = 5) then 2 else 3")
         self.check("if 5<>5 then 2+5 else 5+5", "if (5 <> 5) then (2 + 5) else (5 + 5)")
 
+    def test_let_var(self):
         self.check("let var a := 5 in a end", "let var a: int := 5 in a end")
         self.check("let var a: int := 5 in a end", "let var a: int := 5 in a end")
         self.check("let var a := 5 var b := a in a+b end", "let var a: int := 5 var b: int := a in (a + b) end")
 
+    def test_let_function(self):
         self.check("let function f() = 1+2 in 1 end", "let function f(): int = (1 + 2) in 1 end")
         self.check("let function f() = f() in f() end", "let function f() = f() in f() end")
         self.check("let function f(a:int, b:int) = a+b in f(4,5) end", "let function f(a: int, b: int): int = (a + b) in f(4, 5) end")
         self.check("let function f(): int = 1 + 2 in 1 end", "let function f(): int = (1 + 2) in 1 end")
         self.check("let function f(a: int, b: int, c: int) = 1 in f(1,2,3) end", "let function f(a: int, b: int, c: int): int = 1 in f(1, 2, 3) end")
 
+    def test_code(self):
         self.check("let\n\
                         var a := 3\n\
                         var b : int := 4\n\
@@ -86,9 +90,11 @@ class TestDumper(unittest.TestCase):
                         f(10)\n\
                     end", "let var a/*e*/: int := 3 function f(b: int): int = (a/*1*/ + b) in f(10) end")
 
+    def test_comment(self):
         self.check("1+3 // clqjzdi&é678 dzo!!", "(1 + 3)")
         self.check("1+3 /* duh789 edeu /* clqjzdi&é678 dzo!! */ ùùùù */", "(1 + 3)")
 
+    def test_seqexp(self):
         self.check("let var a := 1 in end", "let var a: int := 1 in () end")
         self.check("let var a := 1 in a end", "let var a: int := 1 in a end")
         self.check("let var a := 1 in (a) end", "let var a: int := 1 in a end")
