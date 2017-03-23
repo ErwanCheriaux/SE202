f1a3737d8179ad1cbb9a4fef443c940f95b3abc1
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-03-23 22:12:09 +0100

    Update binder

--- semantics/binder.py
+++ semantics/binder.py
@@ -117,7 +117,7 @@ class Binder(Visitor):
     @visitor(Identifier)
     def visit(self, id):
         self.lookup(id)
-        if type(id.decl) is not VarDecl and not IndexDecl:
+        if type(id.decl) is not VarDecl and type(id.decl) is not IndexDecl:
             raise BindException("Is not a variable: %s" % id.name)
 
     @visitor(IfThenElse)
