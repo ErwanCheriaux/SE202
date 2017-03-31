19a2e568bbcd568cbb8e5399628fbafce2c4d7bf
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-03-31 17:04:32 +0200

    Ajout class Block - wip

--- ir/blocks.py
+++ ir/blocks.py
@@ -1,6 +1,17 @@
 from ir.nodes import *
 from frame.frame import Frame
 
+class Block:
+    """Block commencant par un label et terminant par un (c)jump"""
+
+    def __init__(self, name, jump, stms):
+        assert isinstance(name, Label), "name must be a Label"
+        assert isinstance(jump, Label), "jump must be a Label"
+        assert isinstance(stms, list), "stms must be a list of Stm"
+        self.name  = name
+        self.jump  = jump
+        self.block = block
+        self.exam  = False
 
 def reorder_blocks(seq, frame):
     """Reorder blocks in seq so that the negative branch of a CJUMP always
