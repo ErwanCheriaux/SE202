8903d41cfe2b286e2a43745fb183d02dd46bc413
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.com>
2017-04-17 20:56:17 +0200

    completion du dico interferences - wip

--- codegen/liveness.py
+++ codegen/liveness.py
@@ -1,4 +1,5 @@
-from codegen.instr import LABEL as L, MOVE as M
+from codegen.instr import LABEL as L, MOVE as M, OPER as O
+from ir.nodes import *
 
 def liveness_analysis(frame, instrs):
     """Annotate instructions with liveness analysis. Return the interferences
@@ -11,7 +12,37 @@ def liveness_analysis(frame, instrs):
     temporaries involved in a direct MOVE operation. Those are susceptible
     of merging."""
 
+    debug = True
+
     interferences = {}
     coalesces = {}
+
+    for i in instrs:
+        # At any nonmove instruction that defines a variable a, where the live-out
+        # variables are b 1 , ... , b j , add interference edges
+        # (a, b 1 ), ... , (a, b j ).
+        if isinstance(i, O) and len(i.dsts) == 1:
+            key = i.dsts[0].name
+            interferences[key] = i.srcs
+
+        # At a move instruction a ← c, where variables b 1 , ... , b j are live-out,
+        # add interference edges (a, b 1 ), ... , (a, b j ) for any b i that is
+        # not the same as c.
+        if isinstance(i, M):
+            pass
+
+    if debug:
+        print("=================== FRAME ====================")
+        print(frame)
+
+        print("=================== INSTRS ===================")
+        for i in instrs:
+            print(i)
+
+        print("=============== INTERFERENCES ================")
+        for key, value in interferences.items():
+            print (key, value)
+
+        print("==============================================")
 
     return interferences, coalesces
